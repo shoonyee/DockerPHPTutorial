804d61bc8f790eafbd7e33c3385dbfb3e3a8d87f
shun han <shunhan@umich.edu>
2020-02-11 10:48:19 -0500

    first commit

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Pets!<hr>";
+$conn = new mysqli("mysql", "root", ".sweetpwd.", "my_db");
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM pet";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+		echo $row['name']." the ".$row['species']." is ".$row['owner']."'s pet.";
+		echo "<br>";
+	}
+} else {
+	echo "0 results";
+}
+$conn->close();
+
+//phpinfo();
+?>
